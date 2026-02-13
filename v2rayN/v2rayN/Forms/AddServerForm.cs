6588035d331b9cf0c1644a98203731d197bef8f4
aiteam8 <aiteam8@hotmail.com>
2026-02-13 12:11:56 +0000

    Add Hysteria2, Mieru, TUIC UI controls in AddServerForm

--- v2rayN/v2rayN/Forms/AddServerForm.cs
+++ v2rayN/v2rayN/Forms/AddServerForm.cs
@@ -9,9 +9,94 @@ namespace v2rayN.Forms
 {
     public partial class AddServerForm : BaseServerForm
     {
+        private TextBox txtUpMbps;
+        private TextBox txtDownMbps;
+        private TextBox txtObfsPassword;
+        private ComboBox cmbObfs;
+        private TextBox txtCertSha256;
+        private Label lblUpMbps, lblDownMbps, lblObfs, lblObfsPassword, lblCertSha256;
+
         public AddServerForm()
         {
             InitializeComponent();
+            AddNewProtocolControls();
+        }
+
+        private void AddNewProtocolControls()
+        {
+            if (eConfigType == EConfigType.Hysteria2 || eConfigType == EConfigType.Mieru || eConfigType == EConfigType.TUIC)
+            {
+                int yPos = 120;
+
+                lblUpMbps = new Label();
+                lblUpMbps.Text = "Up Mbps:";
+                lblUpMbps.Location = new System.Drawing.Point(10, yPos);
+                lblUpMbps.Size = new System.Drawing.Size(70, 20);
+                panVmess.Controls.Add(lblUpMbps);
+
+                txtUpMbps = new TextBox();
+                txtUpMbps.Location = new System.Drawing.Point(85, yPos - 3);
+                txtUpMbps.Size = new System.Drawing.Size(100, 20);
+                txtUpMbps.Text = "100";
+                panVmess.Controls.Add(txtUpMbps);
+
+                yPos += 25;
+                lblDownMbps = new Label();
+                lblDownMbps.Text = "Down Mbps:";
+                lblDownMbps.Location = new System.Drawing.Point(10, yPos);
+                lblDownMbps.Size = new System.Drawing.Size(70, 20);
+                panVmess.Controls.Add(lblDownMbps);
+
+                txtDownMbps = new TextBox();
+                txtDownMbps.Location = new System.Drawing.Point(85, yPos - 3);
+                txtDownMbps.Size = new System.Drawing.Size(100, 20);
+                txtDownMbps.Text = "100";
+                panVmess.Controls.Add(txtDownMbps);
+
+                if (eConfigType == EConfigType.Hysteria2)
+                {
+                    yPos += 25;
+                    lblObfs = new Label();
+                    lblObfs.Text = "Obfs:";
+                    lblObfs.Location = new System.Drawing.Point(10, yPos);
+                    lblObfs.Size = new System.Drawing.Size(70, 20);
+                    panVmess.Controls.Add(lblObfs);
+
+                    cmbObfs = new ComboBox();
+                    cmbObfs.Location = new System.Drawing.Point(85, yPos - 3);
+                    cmbObfs.Size = new System.Drawing.Size(100, 20);
+                    cmbObfs.Items.AddRange(new object[] { "salamander", "" });
+                    cmbObfs.Text = "";
+                    panVmess.Controls.Add(cmbObfs);
+
+                    yPos += 25;
+                    lblObfsPassword = new Label();
+                    lblObfsPassword.Text = "Obfs Pass:";
+                    lblObfsPassword.Location = new System.Drawing.Point(10, yPos);
+                    lblObfsPassword.Size = new System.Drawing.Size(70, 20);
+                    panVmess.Controls.Add(lblObfsPassword);
+
+                    txtObfsPassword = new TextBox();
+                    txtObfsPassword.Location = new System.Drawing.Point(85, yPos - 3);
+                    txtObfsPassword.Size = new System.Drawing.Size(150, 20);
+                    panVmess.Controls.Add(txtObfsPassword);
+                }
+
+                if (eConfigType == EConfigType.TUIC || eConfigType == EConfigType.Hysteria2)
+                {
+                    yPos += 25;
+                    lblCertSha256 = new Label();
+                    lblCertSha256.Text = "Cert SHA256:";
+                    lblCertSha256.Location = new System.Drawing.Point(10, yPos);
+                    lblCertSha256.Size = new System.Drawing.Size(70, 20);
+                    panVmess.Controls.Add(lblCertSha256);
+
+                    txtCertSha256 = new TextBox();
+                    txtCertSha256.Location = new System.Drawing.Point(85, yPos - 3);
+                    txtCertSha256.Size = new System.Drawing.Size(250, 20);
+                    panVmess.Controls.Add(txtCertSha256);
+                }
+            }
         }
 
         private void AddServerForm_Load(object sender, EventArgs e)
@@ -112,6 +197,16 @@ namespace v2rayN.Forms
                     txtId6.Text = vmessItem.id;
                     cmbFlow6.Text = vmessItem.flow;
                     break;
+                case EConfigType.Hysteria2:
+                case EConfigType.Mieru:
+                case EConfigType.TUIC:
+                    txtId.Text = vmessItem.id;
+                    if (txtUpMbps != null) txtUpMbps.Text = vmessItem.upMbps?.ToString() ?? "100";
+                    if (txtDownMbps != null) txtDownMbps.Text = vmessItem.downMbps?.ToString() ?? "100";
+                    if (txtObfsPassword != null) txtObfsPassword.Text = vmessItem.obfsPassword ?? "";
+                    if (cmbObfs != null) cmbObfs.Text = vmessItem.obfs ?? "";
+                    if (txtCertSha256 != null) txtCertSha256.Text = vmessItem.certSha256 ?? "";
+                    break;
             }
 
             cmbCoreType.Text = vmessItem.coreType == null ? string.Empty : vmessItem.coreType.ToString();
@@ -192,6 +287,11 @@ namespace v2rayN.Forms
                     id = txtId6.Text;
                     flow = cmbFlow6.Text;
                     break;
+                case EConfigType.Hysteria2:
+                case EConfigType.Mieru:
+                case EConfigType.TUIC:
+                    id = txtId.Text;
+                    break;
             }
 
             if (Utils.IsNullOrEmpty(address))
@@ -244,6 +344,20 @@ namespace v2rayN.Forms
                 vmessItem.coreType = (ECoreType)Enum.Parse(typeof(ECoreType), cmbCoreType.Text);
             }
 
+            if (eConfigType == EConfigType.Hysteria2 || eConfigType == EConfigType.Mieru || eConfigType == EConfigType.TUIC)
+            {
+                if (txtUpMbps != null && !string.IsNullOrEmpty(txtUpMbps.Text))
+                    vmessItem.upMbps = Utils.ToInt(txtUpMbps.Text);
+                if (txtDownMbps != null && !string.IsNullOrEmpty(txtDownMbps.Text))
+                    vmessItem.downMbps = Utils.ToInt(txtDownMbps.Text);
+                if (txtObfsPassword != null)
+                    vmessItem.obfsPassword = txtObfsPassword.Text;
+                if (cmbObfs != null)
+                    vmessItem.obfs = cmbObfs.Text;
+                if (txtCertSha256 != null)
+                    vmessItem.certSha256 = txtCertSha256.Text;
+            }
+
             int ret = -1;
             switch (eConfigType)
             {
@@ -263,6 +377,11 @@ namespace v2rayN.Forms
                 case EConfigType.Trojan:
                     vmessItem.flow = flow;
                     ret = ConfigHandler.AddTrojanServer(ref config, vmessItem);
+                    break;
+                case EConfigType.Hysteria2:
+                case EConfigType.Mieru:
+                case EConfigType.TUIC:
+                    ret = ConfigHandler.AddServer(ref config, vmessItem);
                     break;
             }
 
